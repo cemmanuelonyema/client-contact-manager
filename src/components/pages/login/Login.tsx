4384cab503dc2617454ba573e891764869604f83
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-07-26 01:38:16 +0100

    add login and signup page

--- src/components/pages/login/Login.tsx
+++ src/components/pages/login/Login.tsx
@@ -1,7 +1,82 @@
-import React from "react";
+import { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import styles from "./login.module.scss";
 
 const Login = () => {
-  return <div>Login</div>;
+  //user states
+  const [user, setUser] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = user;
+  const inputRef = useRef<HTMLInputElement>(null!);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setUser({
+      username: "",
+      password: "",
+    });
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setUser({ ...user, [e.target.name]: e.target.value });
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <section className={styles.signUp}>
+      <div className={`${styles.signContainer} container`}>
+        <div className={styles.formContainer}>
+          <div className={styles.formHeader}>
+            <h1>Welcome back !</h1>
+            <p>Continue with Contact Manager</p>
+          </div>
+
+          <form onSubmit={handleSubmit} className="form">
+            <div className={styles.formGroup}>
+              <label htmlFor="username">Username</label>
+              <input
+                className={styles.input}
+                ref={inputRef}
+                type="text"
+                name="username"
+                value={username}
+                onChange={handleChange}
+                placeholder="your username"
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label className={styles.label} htmlFor=" password">
+                Password
+              </label>
+              <input
+                className={styles.input}
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                placeholder="your password"
+                required
+              />
+            </div>
+
+            <div className={styles.formFooter}>
+              <input className={styles.formBtn} type="submit" value="Login" />
+
+              <span>
+                Don't' have an account? <Link to="/signup">Sign Up</Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Login;
--- src/components/pages/signup/Signup.tsx
+++ src/components/pages/signup/Signup.tsx
@@ -1,7 +1,100 @@
-import React from "react";
+import { useState, useRef, useEffect } from "react";
+import { Link } from "react-router-dom";
+import styles from "./signup.module.scss";
 
 const Signup = () => {
-  return <div>Signup</div>;
+  //user states
+  const [user, setUser] = useState({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const { username, email, password } = user;
+  const inputRef = useRef<HTMLInputElement>(null!);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    setUser({
+      username: "",
+      email: "",
+      password: "",
+    });
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setUser({ ...user, [e.target.name]: e.target.value });
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
+  return (
+    <section className={styles.signUp}>
+      <div className={`${styles.signContainer} container`}>
+        <div className={styles.formContainer}>
+          <div className={styles.formHeader}>
+            <h1>Welcome </h1>
+            <p>Sign up with Contact Manager</p>
+          </div>
+
+          <form onSubmit={handleSubmit} className="form">
+            <div className={styles.formGroup}>
+              <label className={styles.label} htmlFor=" username">
+                username
+              </label>
+              <input
+                className={styles.input}
+                ref={inputRef}
+                type="text"
+                name="username"
+                value={username}
+                onChange={handleChange}
+                placeholder="your username"
+                required
+              />
+            </div>
+            <div className={styles.formGroup}>
+              <label htmlFor="email">Email</label>
+              <input
+                className={styles.input}
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                placeholder="youraddress@mail.com"
+                required
+              />
+            </div>
+
+            <div className={styles.formGroup}>
+              <label className={styles.label} htmlFor=" password">
+                Password
+              </label>
+              <input
+                className={styles.input}
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                placeholder="your password"
+                required
+              />
+            </div>
+
+            <div className={styles.formFooter}>
+              <input className={styles.formBtn} type="submit" value="Sign up" />
+
+              <span>
+                Have an account already? <Link to="/login">Login</Link>
+              </span>
+            </div>
+          </form>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Signup;
