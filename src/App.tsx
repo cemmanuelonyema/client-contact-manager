78c98dbf983fc6c97274bfde641b0e84e93895bb
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-07-26 01:24:00 +0100

    add page router set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,16 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Contacts, Landing, Login, Signup } from "./components/pages";
 
 function App() {
   return (
     <>
-      <h1>HELLOO CONTACT MANAGER</h1>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
     </>
   );
 }
---
+++ src/components/pages/index.ts
@@ -0,0 +1,6 @@
+import Contacts from "./contacts/Contacts";
+import Login from "./login/Login";
+import Signup from "./signup/Signup";
+import Landing from "./landing/Landing";
+
+export { Contacts, Signup, Login, Landing };
