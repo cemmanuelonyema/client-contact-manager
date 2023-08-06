f4e12e031a0844a994b035978c446127ae594cdb
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-08-06 11:31:01 +0100

    modify sign up and login pages

--- src/components/pages/signup/Signup.tsx
+++ src/components/pages/signup/Signup.tsx
@@ -31,67 +31,83 @@ const Signup = () => {
   }, []);
 
   return (
+    // <section className={styles.signUp}>
+    //   <div className={`${styles.signContainer} container`}>
+    //     <div className={styles.formContainer}>
+    //       <div className={styles.formHeader}>
+
+    //       </div>
+
+    //       <form onSubmit={handleSubmit} className="form">
+
+    //         <div className={styles.formFooter}>
+    //           <input className={styles.formBtn} type="submit" value="Sign up" />
+
+    //           <span>
+    //             Have an account already? <Link to="/login">Login</Link>
+    //           </span>
+    //         </div>
+    //       </form>
+    //     </div>
+    //   </div>
+    // </section>
+
     <section className={styles.signUp}>
-      <div className={`${styles.signContainer} container`}>
-        <div className={styles.formContainer}>
-          <div className={styles.formHeader}>
-            <h1>Welcome </h1>
-            <p>Sign up with Contact Manager</p>
+      <div className={styles.signUpContainer}>
+        <div className={styles.signUpHeader}>
+          <h1>Welcome </h1>
+          <p>Sign up with Contact Manager</p>
+        </div>
+        <form className={styles.form}>
+          <div className={styles.formGroup}>
+            <label className={styles.label} htmlFor=" username">
+              username
+            </label>
+            <input
+              className={styles.input}
+              ref={inputRef}
+              type="text"
+              name="username"
+              value={username}
+              onChange={handleChange}
+              placeholder="your username"
+              required
+            />
+          </div>
+          <div className={styles.formGroup}>
+            <label htmlFor="email">Email</label>
+            <input
+              className={styles.input}
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              placeholder="youraddress@mail.com"
+              required
+            />
           </div>
 
-          <form onSubmit={handleSubmit} className="form">
-            <div className={styles.formGroup}>
-              <label className={styles.label} htmlFor=" username">
-                username
-              </label>
-              <input
-                className={styles.input}
-                ref={inputRef}
-                type="text"
-                name="username"
-                value={username}
-                onChange={handleChange}
-                placeholder="your username"
-                required
-              />
-            </div>
-            <div className={styles.formGroup}>
-              <label htmlFor="email">Email</label>
-              <input
-                className={styles.input}
-                type="email"
-                name="email"
-                value={email}
-                onChange={handleChange}
-                placeholder="youraddress@mail.com"
-                required
-              />
-            </div>
-
-            <div className={styles.formGroup}>
-              <label className={styles.label} htmlFor=" password">
-                Password
-              </label>
-              <input
-                className={styles.input}
-                type="password"
-                name="password"
-                value={password}
-                onChange={handleChange}
-                placeholder="your password"
-                required
-              />
-            </div>
-
-            <div className={styles.formFooter}>
-              <input className={styles.formBtn} type="submit" value="Sign up" />
-
-              <span>
-                Have an account already? <Link to="/login">Login</Link>
-              </span>
-            </div>
-          </form>
-        </div>
+          <div className={styles.formGroup}>
+            <label className={styles.label} htmlFor=" password">
+              Password
+            </label>
+            <input
+              className={styles.input}
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              placeholder="your password"
+              required
+            />
+          </div>
+          <div className={styles.signUpFooter}>
+            <input className={styles.formBtn} type="submit" value="Sign up" />
+            <span>
+              Don't' have an account? <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </form>
       </div>
     </section>
   );
